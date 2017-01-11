4d7a4752411d296b3dad02535cb18d22d664e272
karim.berrahil <karim.berrahil@isep.fr>
2017-01-11 17:23:20 +0100

    qjout customer

---
+++ sakila-business-webapi/src/main/resources/static/app/scripts/controllers/customer_controller.js
@@ -0,0 +1,76 @@
+'use strict';
+
+App.controller('CustomerController', ['$scope', '$cookieStore', 'CustomerService', 
+                                      function($scope, $cookieStore, CustomerService) {
+	var self = this;
+	
+	self.customer={customerId:null,store_id:'1',firstName:'',lastName:'',email:'',phone:'',
+			addressId:null ,address:'',address2:'',district:'',city_id:'', postalCode:'',active:null};
+	self.customers=[];
+	
+	self.fetchAllCustomers = function(){
+		CustomerService.getCustomer().then(
+			       function(d) {
+					        self.customers = d;
+					        console.log('success to retrieve all customers');
+				       },
+					function(errResponse){
+						console.error('Error while fetching Currencies');
+					}
+		       );
+	};
+	
+	self.createCustomer = function(customer){
+		CustomerService.createCustomer(customer).then(
+				self.fetchAllCustomers, function(errResponse){
+					console.error('Error while creating Customer.');
+				});
+	};
+
+   self.updateCustomer = function(customer){
+	   console.log("update customer");
+        CustomerService.updateCustomer(customer)
+        .then(
+                self.fetchAllCustomers, 
+			              function(errResponse){
+				               console.error('Error while updating Customer.');
+			              }	
+            );
+    };
+    
+    self.submit = function() {
+        if(self.customer.customerId==null){
+            console.log('Saving New Customer', self.customer); 
+            self.customer['store_id'] = $cookieStore.get('store_id');
+            self.createCustomer(self.customer);
+        }else{
+            console.log('Customer updating with id ', self.customer.customerId);
+            console.log('Customer: ', self.customer);
+            self.updateCustomer(self.customer);
+        }
+        self.reset();
+    };
+    
+    self.edit = function(customerId,addressId){
+        console.log('customer id to be edited', customerId);
+        console.log('address id to be edited', addressId);
+        for(var i = 0; i < self.customers.length; i++){
+            if(self.customers[i].customerId == customerId) {
+            	if (self.customers[i].addressId == addressId){
+            		self.customer = angular.copy(self.customers[i]);
+            		console.log(self.customer);
+                    break;
+            	}
+            }
+        }
+    };
+    
+    self.fetchAllCustomers();
+	
+	self.reset = function(){
+		self.customer={customerId:null,store_id:'1',firstName:'',lastName:'',email:'',phone:'',
+				addressId:null ,address:'',address2:'',district:'',city_id:'', postalCode:'',active:null};
+        $scope.myForm.$setPristine();
+    };
+    
+}]);
